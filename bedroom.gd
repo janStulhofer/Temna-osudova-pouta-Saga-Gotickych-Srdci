5220b0a846441867bc4de250d4f2e6e54e6e2bf7
janStulhofer <148527823+janStulhofer@users.noreply.github.com>
2024-02-28 22:52:24 +0100

    PC Desktop env. v ložnici

--- bedroom.gd
+++ bedroom.gd
@@ -1,29 +1,31 @@
 extends Node2D
 
-var selected = false
+#var selected = false
 
 ####EXIT####
 func _on_exit_mouse_entered():
 	$StinVystup.visible = true
-	
-	pass # Exittt Replace with function body.
 
 func _on_exit_mouse_exited():
 	$StinVystup.visible = false
-	pass # Replace with function body.
 
 func _on_exit_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.pressed:
 			get_tree().quit() #ZMĚNIT SCÉNU**********
-	
-	 # Replace with function body.
 ####EXIT####
 
 ####PC####
 func _on_area_pc_mouse_entered():
-	pass # PCCC Replace with function body.
+	$PCShadow2.visible = true
+	$PCOver.modulate = Color(1.8, 1.8, 1.8)
+	#$PCShadow2.set_z_index(0)
 
+func _on_area_pc_mouse_exited():
+	$PCShadow2.visible = false
+	$PCOver.modulate = Color(1, 1, 1)
 
-
-
+func _on_area_pc_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.pressed:
+			get_tree().change_scene_to_file("res://Assets/Map/Home/Bedroom/PCDesktop/PCDesktop.tscn")
