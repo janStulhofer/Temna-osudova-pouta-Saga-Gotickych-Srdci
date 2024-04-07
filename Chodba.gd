634cc8d9134dbd592ef6e41c563efbaa13aea093
janStulhofer <148527823+janStulhofer@users.noreply.github.com>
2024-04-07 20:17:04 +0200

    Chodba

--- Chodba.gd
+++ Chodba.gd
@@ -10,9 +10,10 @@ func _ready():
 func _process(delta):
 	pass
 
-
+###BEDROOM####
 func _on_pokoj_mouse_entered():
-	pass # Replace with function body.
+	$door2shadow.visible = true
+	$door2.modulate = Color(1.8, 1.8, 1.8)
 
 
 func _on_pokoj_input_event(viewport, event, shape_idx):
@@ -21,7 +22,63 @@ func _on_pokoj_input_event(viewport, event, shape_idx):
 			get_tree().change_scene_to_file("res://bedroom.tscn")
 
 
+func _on_pokoj_mouse_exited():
+	$door2shadow.visible = false
+	$door2.modulate = Color(1, 1, 1)
+
+###BEDROOM###
+
 func _on_area_2d_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.pressed:
 			get_tree().change_scene_to_file("res://map.tscn")
+
+###DOOR3###
+func _on_door_3_mouse_entered():
+	$door3shadow.visible = true
+	$door3.modulate = Color(1.8, 1.8, 1.8)
+
+
+func _on_door_3_mouse_exited():
+	$door3shadow.visible = false
+	$door3.modulate = Color(1, 1, 1)
+
+
+func _on_door_3_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.pressed:
+			get_tree().change_scene_to_file("res://kitchen.tscn")
+
+###DOOR3###
+
+###DOOR1###
+func _on_door_1_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.pressed:
+			get_tree().change_scene_to_file("res://map.tscn")
+
+func _on_door_1_mouse_entered():
+	$door1shadow.visible = true
+	$door1.modulate = Color(1.8, 1.8, 1.8)
+
+
+func _on_door_1_mouse_exited():
+	$door1shadow.visible = false
+	$door1.modulate = Color(1, 1, 1)
+
+###DOOR1###
+
+###PLANT###
+func _on_plan_tarea_input_event(viewport, event, shape_idx):
+	pass # Prida dialog o kytce
+
+
+func _on_plan_tarea_mouse_entered():
+	$plantShadow.visible = true
+	$plant.modulate = Color(1.4, 1.4, 1.4)
+
+
+func _on_plan_tarea_mouse_exited():
+	$plantShadow.visible = false
+	$plant.modulate = Color(1, 1, 1)
+###PLANT###
---
+++ kitchen.gd
@@ -0,0 +1,47 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+###STIN###
+func _on_hallway_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.pressed:
+			get_tree().change_scene_to_file("res://chodba.tscn")
+
+
+func _on_hallway_mouse_entered():
+	$kitchenShadow.visible = true
+
+
+
+func _on_hallway_mouse_exited():
+	$kitchenShadow.visible = false
+
+###STIN###
+
+###FOOD###
+
+###FOOD###
+
+
+
+
+
+func _on_food_input_event(viewport, event, shape_idx):
+	pass # Replace with function body.
+
+
+func _on_food_mouse_entered():
+	pass # Replace with function body.
+
+
+func _on_food_mouse_exited():
+	pass # Replace with function body.
