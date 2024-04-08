b0371d8944f9096e84008f1dafe360e09efb771e
janStulhofer <148527823+janStulhofer@users.noreply.github.com>
2024-04-08 22:21:03 +0200

    Skola, coll. script

--- skola.gd
+++ skola.gd
@@ -10,8 +10,16 @@ func _ready():
 func _process(delta):
 	pass
 
-
+###exit###
 func _on_skola_2_exit_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.pressed:
 			get_tree().change_scene_to_file("res://map.tscn")
+###exit###
+
+###enter###
+
+###enter###
+
+func _on_area_2d_input_event(viewport, event, shape_idx):
+	pass # Replace with function body.
